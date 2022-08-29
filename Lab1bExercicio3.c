f13f211dab3ed2dab6bacaff306fc0eedd044749
Gustavo Aragão Guedes <64610385+Gustavo-Aragao-Guedes@users.noreply.github.com>
2022-08-29 09:35:18 -0300

    Create Lab1bExercicio3.c

---
+++ Lab1bExercicio3.c
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, i, j, l, k;
+  cout << "Insira o número máximo da pirâmide invertida: (Apenas ímpares)" << endl;
+	do{
+	cin >> n;
+	} while(n%2==0);
+    l=0;
+  cout << endl;
+  
+	for(i=1;i<=n;i++){
+    for(k=0;k<l;k++){cout << " ";}
+		cout << i << " ";
+		for(j=i+1;j<=n;j++)
+			cout << j << " ";
+			n-=1;
+			cout << endl;
+      l+=2;
+    }
+
+    return 0;
+}
