51ea695919f2a89a61450a70ddfead64790a13e9
Gustavo Aragão Guedes <64610385+Gustavo-Aragao-Guedes@users.noreply.github.com>
2022-08-29 09:31:04 -0300

    Create Lab01bExercicio1.c

---
+++ Lab01bExercicio1.c
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#define L 5
+#define C 5
+using namespace std;
+
+int main(){
+  int Matriz[L][C], i, j, menorN, menorL;
+  srand(time(NULL));
+  cout << "Matriz quadrada: " << endl;
+  for (i = 0; i < L; i++) {
+        for (j = 0; j < C; j++) {
+            Matriz[i][j] = rand()%100;
+            cout << " | " <<  Matriz[i][j];
+        }
+      cout << endl;
+  }
+  
+  for (i=0; i< L; i++){
+    for (j=0; j< C; j++){
+      if (Matriz[i][j] < menorN) {
+        menorN = Matriz[i][j];
+        menorL = i+1;
+    }
+  }
+}
+
+cout << endl << "A linha que contem o menor valor é: " << menorL;
+  
+}
